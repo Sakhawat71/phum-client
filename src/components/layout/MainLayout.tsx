d3026e26613b9aebee7f4f6ce8ac21b23ef19a3e
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-01-15 23:52:14 +0600

    refactor MainLayout component to use structured menu items and improve layout styling

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,24 +1,39 @@
-import { Layout, Menu } from "antd";
+import { Layout, Menu, MenuProps } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import { createElement } from "react";
 
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-    (icon, index) => ({
-        key: String(index + 1),
-        icon: createElement(icon),
-        label: `nav ${index + 1}`,
-    }),
-);
+
+const items: MenuProps['items'] = [
+    {
+        key: '1',
+        label: 'Dashboard'
+    },
+    {
+        key: '2',
+        label: 'Profile'
+    },
+    {
+        key: '3',
+        label: 'User management',
+        children: [
+            {
+                key: '11',
+                label: 'Create Admin'
+            },
+            {
+                key: '12',
+                label: 'Create User'
+            },
+        ]
+    },
+]
 
 const MainLayout = () => {
 
-    // const {
-    //     token: { colorBgContainer, borderRadiusLG },
-    // } = theme.useToken();
 
     return (
-        <Layout>
+        <Layout style={{ height: '100vh' }}>
             <Sider
                 breakpoint="lg"
                 collapsedWidth="0"
@@ -29,7 +44,18 @@ const MainLayout = () => {
                     console.log(collapsed, type);
                 }}
             >
-                <div className="demo-logo-vertical" />
+                <div
+                    style={{
+                        color: 'white',
+
+                        height: '4rem',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <h1>PH Uni</h1>
+                </div>
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
             </Sider>
 
@@ -51,7 +77,6 @@ const MainLayout = () => {
             </Layout>
 
         </Layout>
-
     );
 };
 
