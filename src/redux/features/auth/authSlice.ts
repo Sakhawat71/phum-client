eca408813c5fc7b0ea7068b8a3dc83afba050bf0
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-01-17 22:10:35 +0600

    auth slice with setUser and logout

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type authState = {
+    user: null | object;
+    token: null | string;
+};
+
+
+const initialState: authState = {
+    user: null,
+    token: null
+}
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setUser: (state, action) => {
+            const { user, token } = action.payload;
+            state.user = user;
+            state.token = token;
+        },
+        logOut: (state, action) => {
+            state.user = null;
+            state.token = null;
+        },
+    },
+});
+
+
+export const { } = authSlice.actions;
+export default authSlice.reducer;
