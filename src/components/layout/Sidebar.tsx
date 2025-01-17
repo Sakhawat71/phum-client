3929d0c8786b0bee56e415cb913804a40685ccde
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-01-17 16:55:42 +0600

    sidebar component

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,36 @@
+import { Menu } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.route";
+
+
+const Sidebar = () => {
+    return (
+        <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={(broken) => {
+                console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+                console.log(collapsed, type);
+            }}
+        >
+            <div
+                style={{
+                    color: 'white',
+
+                    height: '4rem',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <h1>PH Uni</h1>
+            </div>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={sidebarItemsGenerator(adminPaths)} />
+        </Sider>
+    );
+};
+
+export default Sidebar;
