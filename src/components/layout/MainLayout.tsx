4b2839fda586a0b076ca612638cac3c1eacc017c
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-01-20 23:02:11 +0600

    logout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,15 +1,31 @@
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import { Outlet } from "react-router";
 import Sidebar from "./Sidebar";
+import { useAppDispatch } from "../../redux/hooks";
+import { logOut } from "../../redux/features/auth/authSlice";
 const { Header, Content } = Layout;
 
 
 const MainLayout = () => {
+
+    const dispatch = useAppDispatch();
+    const handelLogOut = () => {
+        dispatch(logOut())
+    };
+
+
     return (
         <Layout style={{ height: '100vh' }}>
             <Sidebar />
             <Layout>
-                <Header style={{ padding: 0, }} />
+                <Header style={{ padding: 0, display: 'flex', justifyContent: 'right', alignContent: 'center', paddingRight: '50px' }} >
+                    <div>
+                        <Button
+                            style={{ color: 'white', background: '#001529' }}
+                            onClick={handelLogOut}
+                        >Log Out</Button>
+                    </div>
+                </Header>
                 <Content style={{ margin: '24px 16px 0' }}>
                     <div
                         style={{
@@ -20,7 +36,7 @@ const MainLayout = () => {
                         <Outlet />
                     </div>
                 </Content>
-                
+
             </Layout>
 
         </Layout>
